358b5a1ea9afb71111257a852634870d15b221c9
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2020-11-02 12:44:14 -0600

    added template for main class

---
+++ src/GGClient.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/** main class for running the GG client application */
+public class GGClient extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
