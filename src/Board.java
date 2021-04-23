1f8072602f3be4241dbaed45995b2ac869463d57
Olivia Orf <oorf@missouriwestern.edu>
2021-04-23 11:53:33 -0500

    Started parsing board description

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import javafx.scene.layout.GridPane;
 import java.io.IOException;
 
@@ -5,12 +7,15 @@ public class Board extends GridPane {
 
     public Board(GameConnection connection, String boardName)  {
         var jsonDescription = connection.getBoardJSON(boardName);
+        Gson parser = new Gson();
+        var boardInfo = parser.fromJson(jsonDescription, JsonObject.class);
+
         // TODO: parse json
         // TODO: use json parameters to initialize board
         // TODO: use
 
-        final int boardWidth = 20;
-        final int boardHeight = 12;
+        final int boardWidth = boardInfo.get("width").getAsInt();
+        final int boardHeight = boardInfo.get("height").getAsInt();
 
         for(int row=0; row<boardHeight; row++) {
             for (int col = 0; col < boardWidth; col++) {
