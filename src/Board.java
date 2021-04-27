f1aca57aed7d3f900eb98550be38ddfc7ffe83b7
Olivia Orf <oorf@missouriwestern.edu>
2021-04-27 15:54:32 -0500

    json parsing

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import javafx.scene.layout.GridPane;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Board extends GridPane {
 
@@ -12,17 +13,23 @@ public class Board extends GridPane {
 
         // TODO: parse json
         // TODO: use json parameters to initialize board
-        // TODO: use
 
         final int boardWidth = boardInfo.get("width").getAsInt();
         final int boardHeight = boardInfo.get("height").getAsInt();
+        final JsonObject tileTypes = boardInfo.get("tileTypes").getAsJsonObject();
+        final HashMap<String,String> tileHashMap = new HashMap<>();
+        for(String key : tileTypes.keySet()){
+            String value = tileTypes.get(key).getAsString();
+            tileHashMap.put(key,value);
+        }
+        final String tilemap = boardInfo.get("tilemap").getAsString();
 
         for(int row=0; row<boardHeight; row++) {
             for (int col = 0; col < boardWidth; col++) {
                 Tile sample = null;
                 {
                     try {
-                        sample = new Tile(connection, "grass");
+                        sample = new Tile(connection, tileHashMap.get(""+tilemap.charAt(col+(boardWidth+1)*row)));
                         add(sample, 0, 0);
                     } catch (IOException e) {
                         e.printStackTrace();
